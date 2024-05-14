0efe13d51f887e5765ef6a2aa125f3a476fdfa22
rayed152 <rayed152@gmail.com>
2024-05-14 13:48:11 +0600

    added changes input component

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -4,105 +4,55 @@
 
   export let value: string;
   export let placeholder: string;
-  export let addonLeft: any  = '';
-  export let addonRight: any = '';
-  export let addonAlt: string = "";
   export let hint: string = '';
-  export let error: string = '';
+  // export let error: string = '';
   export let labelName: string = '';
   export let disabled: boolean = false; 
   export let roundedStyle: string = 'rounded';
-  export let type: string = 'password';
-  export let backgroundColor: string = "bg-green-500"
+  export let type: any = 'email';
+  
 
-  const dispatch = createEventDispatcher();
-
-  const dispatchValue = (e: Event) => {
-    const target = e.target as HTMLInputElement;
-    value = target.value;
-    dispatch('input', value);
-  };
-
-   // Function to validate email format
-   const isValidEmail = (email: string): boolean => {
-      // Regular expression for email validation
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return emailRegex.test(email);
-  };
+  //  // Function to validate email format
+  //  const isValidEmail = (email: string): boolean => {
+  //     // Regular expression for email validation
+  //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;s
+  //     return emailRegex.test(email);
+  // };
 
 </script>
 
-<div class="
-{
-  backgroundColor === "bg-gray-600"? "bg-gray-600" :
-  "bg-green-500"
-}
-relative">
+<div class="relative ">
+ 
   {#if labelName}
     <div>
       <label for={labelName}>{labelName}</label>
     </div>
   {/if}
-  {#if addonLeft}
-      <div class="absolute pl-2 py-2">
-          <!-- {#if typeof addonLeft === 'string'}
-              <span class="text-gray-500">{addonLeft}</span>
-          {:else}
-              <img src={addonLeft} alt="logo" width="24" height="24"/>
-          {/if} -->
-          <img src={addonLeft} alt={addonAlt} width="24" height="24"/>
-      </div>
-  {/if}
-  {#if addonRight}
-      <div class="absolute right-0 pr-1 py-2" >
-          <!-- {#if typeof addonRight === 'string'}
-              <span class="text-gray-500">{addonRight}</span>
-          {:else}
-            <img src={addonLeft} alt="logo" width="24" height="24"/>
-          {/if} -->
-          <img src={addonRight} alt={addonAlt} width="24" height="24"/>
-      </div>
-  {/if}
-  {#if disabled}
-      <input 
-          id={labelName} 
-          type="email"
-          bind:value={value} 
-          placeholder={placeholder} 
-          on:input={dispatchValue} 
-          class="
-          {
-          roundedStyle === 'rounded-xl' ? 'rounded-xl' : 'rounded'
-          } 
-          pl-10 pr-4 py-2  border border-gray-300 focus:border-blue-500 focus:outline-none {backgroundColor}" 
-          disabled 
-      />
-  {:else}
-      <input 
-          id={labelName} 
-          type="email" 
-          bind:value={value} 
-          placeholder={placeholder} 
-         on:input={dispatchValue} 
-          class=
-          "
-          {
-            roundedStyle === 'rounded-xl' ? 'rounded-xl' : 
-            roundedStyle === 'rounded-lg' ? 'rounded-lg' :
-            roundedStyle === 'rounded-md' ? 'rounded-md' :
-            roundedStyle === 'rounded-sm' ? 'rounded-sm' :
-            'rounded'
-            } 
-          pl-10 pr-4 py-2  border border-gray-300 focus:border-blue-500 focus:outline-none" 
-      />
-  {/if}
   
+      <div class="absolute inset-y-0 flex items-center pl-1 w-8">
+        <slot name="leftAddon"/>
+      </div>
+
+      <div class="absolute inset-y-0 right-0 flex items-center w-8 pr-1" >
+        <slot name="rightAddon"/>
+      </div>
+
+      <input 
+          id={labelName} 
+          {...{type}}
+          bind:value={value} 
+          placeholder={placeholder} 
+          class= "
+          {roundedStyle}  
+           px-4 py-2  border border-gray-300 focus:border-blue-500 focus:outline-none {$$slots.leftAddon && "pl-10" } {$$slots.rightAddon ? "pr-10" : " "} "  
+          {disabled}
+      />
  
   {#if hint}
-    <p class="mt-1 text-xs text-white">{hint}</p>
+    <p class="mt-1 text-xs">{hint}</p>
   {/if}
 
-  {#if error && value.trim() !== '' && !isValidEmail(value)} 
+  <!-- {#if error && value.trim() !== '' && !isValidEmail(value)} 
     <p class="mt-1 text-xs text-red-500">{error}</p>
-  {/if}
+  {/if} -->
 </div>
--- src/routes/input/+page.svelte
+++ src/routes/input/+page.svelte
@@ -2,33 +2,24 @@
     import { createEventDispatcher } from 'svelte';
     import Input from '../../components/Input.svelte';
     import email from "$lib/assets/email.png";
-
     let inputValue = '';
     let placeholder = 'Enter Email...';
     // let hint = 'This is a hint.';
-    let error = 'Not an Email..';
-    let addon = email;
-    let addonRight = "hello";
-    let addonAlt = "$";
-    const dispatch = createEventDispatcher();
-    $: console.log(inputValue);
+   
   </script>
   
   <div class="flex justify-center items-center h-screen">
     <Input 
     labelName={"Email"}
-    
-    addonRight={addon}
-    addonAlt = {addonAlt}
+    type={"email"}
     bind:value={inputValue} 
     placeholder={placeholder} 
-    hint={"hello World"} 
-    error={error} 
-    on:input={(e) => console.log(e.detail)
-    }
-    roundedStyle="rounded-xl"
-    backgroundColor="bg-gray-600"
-    />
+    hint={"This is a hint"} 
+    roundedStyle="rounded-lg"
+    >
+    <img slot="leftAddon" src={email} alt="dasdsda"/>
+    <!-- <img slot="rightAddon" src={email} alt="dasdsda"/> -->
+  </Input>
   </div>
   
 
